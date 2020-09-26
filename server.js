d18b77e0e5ddc43af60282989ddc7e859403dbb1
Davi <davibomfimsantiago@gmail.com>
2020-09-26 14:03:31 -0300

    d

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const mysql = require('mysql2')
+//const mysql = require('mysql2')
 
-const connection = mysql.createConnection({
+/*const connection = mysql.createConnection({
     host: '',
     user: 'b97c8bcecb7633',
     database: 'heroku_e881adefe6fbcfa',
     password: 'e9b02acd',
     multipleStatements: true
-})
+})*/
 //teste
 
 app.get('/products', (req, res) => {
