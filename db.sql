d19f6a7f4db9af7d008a4228fd4050bc3905bbdb
Davi <davibomfimsantiago@gmail.com>
2020-09-26 14:58:59 -0300

    db

---
+++ db.sql
@@ -0,0 +1,11 @@
+USE heroku_e881adefe6fbcfa;
+
+CREATE TABLE products (
+    id int(4) AUTO_INCREMENT,
+    picture image,
+    title varchar(255),
+    type varchar(255),
+    rating varchar(255),
+    price varchar(255),
+    created varchar(255)
+);
--- server.js
+++ server.js
@@ -1,16 +1,32 @@
 const express = require('express')
 const app = express()
 const port = 3000
-//const mysql = require('mysql2')
+const mysql = require('mysql2')
 
-/*const connection = mysql.createConnection({
-    host: '',
+const connection = mysql.createConnection({
+    host: 'us-cdbr-east-02.cleardb.com',
     user: 'b97c8bcecb7633',
     database: 'heroku_e881adefe6fbcfa',
     password: 'e9b02acd',
     multipleStatements: true
-})*/
-//teste
+})
+
+connection.connect(err => {
+  if (!err) {
+    console.log('Conectado ao banco de dados.')
+  }
+  if (err) {
+    console.log('Não foi possivel conectar ao banco de dados.')
+  }
+})
+
+connection.query(
+  'SELECT * FROM products;',
+  function(err, results, fields) {
+    console.log(results); // results contains rows returned by server
+    console.log(fields); // fields contains extra meta data about results, if available
+  }
+);
 
 app.get('/products', (req, res) => {
     res.status(200).send('Relação dos Produtos')
